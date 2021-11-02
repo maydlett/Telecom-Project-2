f26edfdeb90ce7f24369a9fc399b158384a549b3
bandademan <93277539+maydlett@users.noreply.github.com>
2021-11-02 09:08:38 -0400

    Updated SQL with phone number
    Added the phone number of each device to the database

--- SQL/Current SQL.sql
+++ SQL/Current SQL.sql
@@ -18,6 +18,7 @@ CREATE TABLE `telecom`.`devices` (
   `device_id` INT NOT NULL AUTO_INCREMENT,
   `device_name` VARCHAR(45) NULL,
   `device_type` VARCHAR(45) NULL,
+  `device_number` VARCHAR(45) NOT NULL,
   `plan_id` INT NOT NULL,
   PRIMARY KEY (`device_id`),
   FOREIGN KEY (`plan_id`) REFERENCES `plans` (`plan_id`));
@@ -41,10 +42,10 @@ INSERT INTO plans (plan_id, plan_name, plan_price, plan_numlines, customer_id) V
 
 SELECT * FROM devices;
 
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (1, "Matt's Phone", "Apple 11", 3);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (2, "Doug's Phone", "Android", 5);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (3, "Zach's Phone", "Nokia", 4);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (4, "Burner Phone 1", "Samsung", 1);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (5, "Burner Phone 2", "Samsung", 1);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (6, "Burner Phone 3", "Samsung", 1);
-INSERT INTO devices (device_id, device_name, device_type, plan_id) VALUES (7, "Burner Phone 4", "Samsung", 1);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (1, "Matt's Phone", "Apple 11", "111-222-3333", 3);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (2, "Doug's Phone", "Android", "123-123-1234", 5);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (3, "Zach's Phone", "Nokia", "111-234-2345", 4);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (4, "Burner Phone 1", "Samsung", "999-888-7777", 1);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (5, "Burner Phone 2", "Samsung", "456-444-3333", 1);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (6, "Jenny's Phone", "Samsung", "111-867-5309", 1);
+INSERT INTO devices (device_id, device_name, device_type, device_number, plan_id) VALUES (7, "Burner Phone 4", "Samsung", "888-332-1122", 1);
